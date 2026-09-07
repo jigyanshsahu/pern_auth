e53f7202674d8f036b6030a9bee2ff8ae8ff2c84
Jigyansh sahu <indianinja008@gmail.com>
2026-09-07 22:46:35 +0530

    feat: implement main application structure with routing and user authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,47 @@
-
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+axios.defaults.withCredentials = true;
 const App = () => {
-  return (
-    <div className="  bg-red-500">
-      hello
-    </div>
-  )
-}
+  const [user, setuser] = useState(null);
+  const [error, seterror] = useState("");
+  const [loading, setloading] = useState(true);
 
-export default App
+  useEffect(() => {
+    const fetchuser = async () => {
+      try {
+        const res = await axios.get("http://localhost:5000/api/auth/me");
+        setuser(res.data);
+      } catch (err) {
+        setuser(null);
+      } finally {
+        setloading(false);
+      }
+    };
+    fetchuser();
+  }, []);
+if(loading){
+  return <div>loading...</div>
+}
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
